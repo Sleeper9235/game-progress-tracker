f7235aa66b2e80ffbf8b91b91d23b4f329d49c53
Sleeper9235 <kylejyoumans@gmail.com>
2024-07-15 11:38:41 -0500

    cleaned code

--- controllers/profile.js
+++ controllers/profile.js
@@ -105,14 +105,11 @@ router.get('/:gameId/edit', async (req, res) => {
 router.put('/:gameId', async (req, res) => {
     try {
         const userInDatabase = await User.findOne({ _id: req.session.user._id }).populate('profile')
-        console.log(userInDatabase)
         const currentGame = await Game.findOne({_id: req.params.gameId})
-        console.log(currentGame)
         const myProfile = await Profile.findByIdAndUpdate(userInDatabase.profile._id, { 
             $push: { games: currentGame },
         });
         res.redirect(`/profiles/myProfile`)
-        console.log(myProfile)
     } catch (err) {
         console.log(err)
         res.redirect('/')
@@ -122,13 +119,10 @@ router.put('/:gameId', async (req, res) => {
 router.delete('/:gameId', async (req, res) => {
     try {
         const userInDatabase = await User.findOne({ _id: req.session.user._id }).populate('profile')
-        console.log(userInDatabase)
         const currentGame = await Game.findOne({_id: req.params.gameId})
-        console.log(currentGame)
         const myProfile = await Profile.findByIdAndUpdate(userInDatabase.profile._id, { 
             $pull: { games: currentGame._id },
         });
-        console.log(myProfile)
         res.redirect(`/profiles/myProfile`)
     } catch (err) {
         console.log(err)
